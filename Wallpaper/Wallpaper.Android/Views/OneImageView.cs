05be66d013e9df283036fdaf00a5b102d22ba274
izaporozhchenko <izaporozhchenko@cogniance.com>
2017-04-04 14:55:14 +0300

    Add changing save button color if it is not enabled

--- Wallpaper/Wallpaper.Android/Views/OneImageView.cs
+++ Wallpaper/Wallpaper.Android/Views/OneImageView.cs
@@ -6,6 +6,7 @@ using Android.Widget;
 using MvvmCross.Droid.Views;
 using Wallpaper.ViewModels;
 using MvvmCross.Platform.WeakSubscription;
+using Android.Graphics;
 
 namespace Wallpaper.Droid.Views
 {
@@ -39,6 +40,21 @@ namespace Wallpaper.Droid.Views
             var saveButton = menu.FindItem(Resource.Id.menu_save);
             saveButton.SetEnabled(ViewModel.IsSaveImageEnabled);
             return base.OnCreateOptionsMenu(menu);
+        }
+
+        public override bool OnPrepareOptionsMenu(IMenu menu)
+        {
+            var saveButton = menu.FindItem(Resource.Id.menu_save);
+
+            //change icon if button is not enabled
+            var icon = GetDrawable(Resource.Drawable.ic_save_black_24dp);
+            if (!saveButton.IsEnabled)
+            {
+                icon.Mutate().SetColorFilter(Color.Rgb(150, 150, 150), PorterDuff.Mode.SrcIn);
+            }
+
+            saveButton.SetIcon(icon);
+            return base.OnPrepareOptionsMenu(menu);
         }
 
         public override bool OnOptionsItemSelected(IMenuItem item)
