b50041c907fd6b6a3b62ebda53d0f11910f8da1a
Ivan Zaporozhchenko <ivanzaporozhchenko@gmail.com>
2017-04-13 07:00:38 -0700

    Added iOS views

---
+++ Wallpaper/Wallpaper.iOS/Views/MainView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.iOS.Views;
+using UIKit;
+
+
+namespace Wallpaper.iOS
+{
+	public partial class MainView : MvxViewController
+	{
+		public MainView() : base("MainView", null)
+		{
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+
+			var set = this.CreateBindingSet<MainView, ViewModels.MainViewModel>();
+			set.Apply();
+		}
+	}
+}
+
