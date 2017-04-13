703218f8c7d6f535789aace9e1f52b1224e84a92
Ivan Zaporozhchenko <ivanzaporozhchenko@gmail.com>
2017-04-13 06:59:52 -0700

    Working on iOS solution

--- Wallpaper/Wallpaper.iOS/AppDelegate.cs
+++ Wallpaper/Wallpaper.iOS/AppDelegate.cs
@@ -9,29 +9,29 @@ namespace Wallpaper.iOS
 {
     // The UIApplicationDelegate for the application. This class is responsible for launching the
     // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
-    [Register("AppDelegate")]
-    public partial class AppDelegate : MvxApplicationDelegate
-    {
-        public override UIWindow Window
-        {
-            get;
-            set;
-        }
+	[Register("AppDelegate")]
+	public partial class AppDelegate : MvxApplicationDelegate
+	{
+		public override UIWindow Window
+		{
+			get;
+			set;
+		}
 
-        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
-        {
-            Window = new UIWindow(UIScreen.MainScreen.Bounds);
+		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+		{
+			Window = new UIWindow(UIScreen.MainScreen.Bounds);
 
-            var setup = new Setup(this, Window);
-            setup.Initialize();
+			var setup = new Setup(this, Window);
+			setup.Initialize();
 
-            var startup = Mvx.Resolve<IMvxAppStart>();
-            startup.Start();
+			var startup = Mvx.Resolve<IMvxAppStart>();
+			startup.Start();
 
-            Window.MakeKeyAndVisible();
+			Window.MakeKeyAndVisible();
 
-            return true;
-        }
+			return true;
+	 	}
     }
 }
 
