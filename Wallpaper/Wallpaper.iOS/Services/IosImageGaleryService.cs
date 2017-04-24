8798187ae31f137fe6c6f13658008e8e3222d325
Ivan Zaporozhchenko <ivanzaporozhchenko@gmail.com>
2017-04-24 02:28:04 -0700

    Working on saving image to gallery and signing iOS app

--- Wallpaper/Wallpaper.iOS/Services/IosImageGaleryService.cs
+++ Wallpaper/Wallpaper.iOS/Services/IosImageGaleryService.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using AssetsLibrary;
+using Foundation;
+using UIKit;
 using Wallpaper.Services.Interfaces;
 
 namespace Wallpaper.iOS.Services
@@ -6,12 +12,34 @@ namespace Wallpaper.iOS.Services
     {
         public bool IsImageExist(string fileName)
         {           
+            try
+            {
+                return File.Exists(fileName);
+            }
+            catch (Exception ex)
+            {
+				Console.WriteLine(ex.Message);
+            }
+
             return true;
         }
 
         public void SaveImageToLibrary(string fileName, byte[] imageData)
         {
-            
-        }		       
+			try
+			{
+				var nsData = NSData.FromArray(imageData);
+				var uiImage = UIImage.LoadFromData(nsData);
+				var meta = new NSDictionary("UIImagePickerControllerMediaMetadata", fileName);
+				var library = new ALAssetsLibrary();
+				library.WriteImageToSavedPhotosAlbum (uiImage.CGImage, null, (assetUrl, error) =>{
+        			Console.WriteLine ("assetUrl:"+assetUrl);
+    			});
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+        }		 
     }
 }
